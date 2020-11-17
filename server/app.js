3036851a7f3ec0f7b0e100e480d2e1d84a9406a1
DrLiamKless <67854560+DrLiamKless@users.noreply.github.com>
2020-11-17 16:07:54 +0200

    can run on build

--- server/app.js
+++ server/app.js
@@ -5,6 +5,13 @@ app.use(express.json())
 
 app.use("/api/v1", require('./api/v1'));
 
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 module.exports = app;
